979dbca72574db6549341bd7d4d6f9e5c66f5204
adis azhar <adisazhar123@gmail.com>
2018-09-06 04:05:20 +0700

    save transaction

--- resources/views/transaksi.blade.php
+++ resources/views/transaksi.blade.php
@@ -139,6 +139,31 @@
     </div>
   </div>
 </section>
+
+<div class="container-fluid">
+  <div class="modal fade after-transaction" tabindex="-1" data-keyboard="false" data-backdrop="static" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel"></h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          ...
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+  </div>
+</div>
+
+</div>
+
+
 @endsection
 
 @section('script')
@@ -247,8 +272,11 @@
           processData: false,
           contentType: false,
           data: formData,
+          dataType: 'JSON',
           success: data => {
-
+              if (data.message == "success") {
+                $(".after-transaction").modal('show');
+              }
           },
           error: data => {
 
